2fc552e1817bf10b7d1436a13f39d58da5c4bd1a
daifeifan <2595039596@qq.com>
2018-04-28 11:44:51 +0800

    demo

---
+++ angularDemo/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', loadChildren: () => new Promise(resolve => {
+    (require as any).ensure([], (require: any) => {
+      resolve(require('./about/about.module').AboutModule);
+    });
+  })}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
